6977b917b8ab2b328107d03bd75e59151c04e3cd
gfranciscoerazom <gfranciscoerazom@protonmail.com>
2021-08-19 07:20:01 -0500

    Creacion de bibliotecas

---
+++ bibliotecasPropias/myLib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "myLib.h"
+
+void hello(const char *name)
+{
+    printf("Hola %s", name);
+}
---
+++ bibliotecasPropias/myLibUsage.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "myLib.h"
+
+int main(int argc, const char *argv[])
+{
+    hello(argv[1]);
+
+    return 0;
+}
